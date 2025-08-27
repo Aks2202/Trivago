bae5a54a9a8ca5e58b435806ef2441990c8a6954
Aks2202 <akshathajn@gmail.com>
2025-08-27 18:25:21 +0200

    Add logs and comments

--- cypress/e2e/validateMap.cy.js
+++ cypress/e2e/validateMap.cy.js
@@ -1,33 +1,47 @@
-const SEARCH_INPUT ='Berlin'
+const SEARCH_INPUT = "Berlin";
+
 describe("Validate hotel markers in map", () => {
   beforeEach(() => {
-    cy.log("Visit the main page and validate consent pop up")
+    cy.log("Visit the main page and validate consent pop up");
     cy.visit("https://www.trivago.com");
-    cy.get("*").shadow({timeout:20000}).find("#deny").click();
+
+    // Verify consent pop up if displayed
+    cy.get("*")
+      .shadow()
+      .find("#deny") // or whatever is inside the shadow root
+      .then(($consent) => {
+        cy.wrap($consent).click({ force: true });
+      });
   });
 
   it("should be possible to view map while searching the hotels in given location", () => {
-    cy.log("Enter the destination and select from suggested options")
-    cy.get('[data-testid="search-form-destination"]',{timeout:20000}).clear().type(SEARCH_INPUT);
+    const SEARCH = '[data-testid="search-form-destination"]';
+    cy.log("Enter the destination and select from suggested options");
+
+    cy.get('[data-testid="search-form-destination"]')
+      .clear()
+      .type(SEARCH_INPUT)
+      .should("have.value", SEARCH_INPUT);
     cy.get('[data-testid="search-suggestions"]').should("be.visible");
 
-    cy.log('Validate that suggestions match SEARCH_INPUT')
+    cy.log("Validate that suggestions match SEARCH_INPUT");
     cy.get('[data-testid="suggestion-title"]')
-      .should("be.visible")
+      .should("not.be.hidden")
       .then(($searchResults) => {
         expect($searchResults).to.contain(SEARCH_INPUT);
-        cy.get($searchResults).first().click({ force: true });
-        cy.get("[data-testid=search-form-destination]").should(
-          "have.value",
-          "Berlin"
-        );
+        cy.wrap($searchResults)
+          .parentsUntil("li")
+          .first()
+          .click({ force: true });
       });
-    cy.get('[data-testid="search-button-with-loader"]').click();
+    cy.get('[data-testid="search-button-with-loader"]').click({ force: true });
 
-    // Validate Hotel list is displayed 
+    // Validate Hotel list is displayed
     cy.get('[data-testid="accommodation-list-element"]').should("exist");
 
-    cy.log("Verify hotel markers are displayed and validate it matches the hotel count")
+    cy.log(
+      "Verify hotel markers are displayed and validate it matches the hotel count"
+    );
     cy.get('[data-testid="map-container"]').should("be.visible");
     cy.get('[data-testid="map-marker"]')
       .should("be.visible")

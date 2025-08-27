bd2179f07fd3b159a6bb4808424cdf79af5d680a
Aks2202 <akshathajn@gmail.com>
2025-08-27 13:54:12 +0200

    final commit

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   viewportWidth: 1280,
   viewportHeight: 720,
-  defaultCommandTimeout: 60000,
+  defaultCommandTimeout: 60000 ,
   e2e: {
     setupNodeEvents(on, config) {
     },
--- cypress/e2e/validateMap.cy.js
+++ cypress/e2e/validateMap.cy.js
@@ -1,32 +1,44 @@
-describe('Validate map', () => {
+const SEARCH_INPUT ='Berlin'
+describe("Validate hotel markers in map", () => {
   beforeEach(() => {
-    cy.clearCookies();
-    cy.visit('https://www.trivago.com'); 
-cy.get('*').shadow().find('#deny').click()
-});
-  it('should be possible to view map when searching in particular location', () => {
-  cy.get('[data-testid="search-form-destination"]').clear().type('berlin');
-  cy.get('[data-testid="search-suggestions"]').should('be.visible');
-  cy.get('[data-testid="suggestion-title"]').should('be.visible').then(($searchResults)=>{
-expect($searchResults).to.contain('Berlin')
-cy.get($searchResults).first().click({force:true})
-cy.get('[data-testid=search-form-destination]').should('have.value','Berlin')
-       })
-  cy.get('[data-testid="search-button-with-loader"]').click();
-//   cy.get('[data-testid="view-map-button"]',{timeout:20000}).click();
-   
-// const today = new Date() // get today's date
-// const tomorrow = new Date(today)
-//       cy.get('[data-testid="search-form-calendar-checkin-value"]').invoke('text',today)
-//        cy.get('[data-testid="search-form-calendar-checkout-value"]').invoke('text',tomorrow)
+    cy.log("Visit the main page and validate consent pop up")
+    cy.visit("https://www.trivago.com");
+    cy.get("*").shadow({timeout:20000}).find("#deny").click();
+  });
 
-       cy.get('[data-testid="accommodation-list-element"]').should('exist')
-   cy.get('[data-testid="map-container"]').should('be.visible');
-    cy.get('[data-testid="map-marker"]').should('be.visible').then((markers) => {
-  const hotelCount = markers.length;
-    // Get the number of expected prices and assert that it matches the hotel count
-  cy.get('[data-testid="expected-price"]').should('have.length', hotelCount);
+  it("should be possible to view map while searching the hotels in given location", () => {
+    cy.log("Enter the destination and select from suggested options")
+    cy.get('[data-testid="search-form-destination"]',{timeout:20000}).clear().type(SEARCH_INPUT);
+    cy.get('[data-testid="search-suggestions"]').should("be.visible");
 
-});
-});
+    cy.log('Validate that suggestions match SEARCH_INPUT')
+    cy.get('[data-testid="suggestion-title"]')
+      .should("be.visible")
+      .then(($searchResults) => {
+        expect($searchResults).to.contain(SEARCH_INPUT);
+        cy.get($searchResults).first().click({ force: true });
+        cy.get("[data-testid=search-form-destination]").should(
+          "have.value",
+          "Berlin"
+        );
+      });
+    cy.get('[data-testid="search-button-with-loader"]').click();
+
+    // Validate Hotel list is displayed 
+    cy.get('[data-testid="accommodation-list-element"]').should("exist");
+
+    cy.log("Verify hotel markers are displayed and validate it matches the hotel count")
+    cy.get('[data-testid="map-container"]').should("be.visible");
+    cy.get('[data-testid="map-marker"]')
+      .should("be.visible")
+      .then((mapMarkers) => {
+        const hotelMarkers = mapMarkers.length;
+
+        // fetch the count of 'expected prices' and assert that it matches the hotel count
+        cy.get('[data-testid="expected-price"]').should(
+          "have.length",
+          hotelMarkers
+        );
+      });
+  });
 });
